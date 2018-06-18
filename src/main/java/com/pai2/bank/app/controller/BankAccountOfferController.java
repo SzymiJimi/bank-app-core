ccf420f2d9fc21d77d02b36f6de68a866cbfa5bb
Szymon Dudek <szymek23q@gmail.com>
2018-06-19 01:53:39 +0200

    BankOffer functionality

---
+++ src/main/java/com/pai2/bank/app/controller/BankAccountOfferController.java
@@ -0,0 +1,47 @@
+package com.pai2.bank.app.controller;
+
+import com.pai2.bank.app.dao.BankAccountOfferDao;
+import com.pai2.bank.app.dao.ManagerDAO;
+import com.pai2.bank.app.dao.PersonDAO;
+import com.pai2.bank.app.dao.implementation.BankAccountOfferDaoImpl;
+import com.pai2.bank.app.dao.implementation.PersonDAOImpl;
+import com.pai2.bank.app.model.Bankaccountoffer;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.mvc.Controller;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.awt.*;
+
+/**
+ * Created by Szymon on 18.06.2018.
+ */
+
+@Controller
+@Stateless
+@Path("offer")
+@Produces((MediaType.APPLICATION_JSON))
+public class BankAccountOfferController {
+    @EJB(beanInterface = PersonDAO.class , beanName = "PersonDAOImpl")
+    private PersonDAO personDAO;
+
+    @EJB(beanInterface = ManagerDAO.class , beanName = "ManagerDaoImpl")
+    private ManagerDAO managerDAO;
+
+    @EJB(beanInterface = BankAccountOfferDao.class, beanName = "BankAccountOfferDaoImpl")
+     private BankAccountOfferDao bankAccountOfferDao;
+
+    @Path("new")
+    @POST
+    @Produces((MediaType.APPLICATION_JSON))
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response saveTest(Bankaccountoffer ob){
+        System.out.println("dotarlem");
+        bankAccountOfferDao.save(ob);
+        System.out.println("zapisalem" + ob);
+        return Response.ok(ob).build();
+
+    }
+}
--- src/main/java/com/pai2/bank/app/dao/implementation/BankAccountOfferDaoImpl.java
+++ src/main/java/com/pai2/bank/app/dao/implementation/BankAccountOfferDaoImpl.java
@@ -3,6 +3,9 @@ package com.pai2.bank.app.dao.implementation;
 import com.pai2.bank.app.dao.BankAccountOfferDao;
 import com.pai2.bank.app.model.Bankaccountoffer;
 
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +13,14 @@ import java.util.List;
 /**
  * Created by Szymon on 14.06.2018.
  */
+@Stateless(name = "BankAccountOfferDaoImpl" , mappedName = "BankAccountOfferDaoImpl")
 public class BankAccountOfferDaoImpl extends DaoImpl<Integer, Bankaccountoffer> implements BankAccountOfferDao {
 
+    @PersistenceContext(unitName = "bank-app")
+    private EntityManager entityManager;
+
+    public void save(Bankaccountoffer bankaccountoffer){{
+        entityManager.persist(bankaccountoffer);
+    }
+    }
 }
