6a557c2b54b0fb1cdc473e32cbd00da638cc3c0a
Szymon Dudek <szymek23q@gmail.com>
2018-06-19 03:56:56 +0200

    Working on registry

--- src/main/java/com/pai2/bank/app/dao/PersonDAO.java
+++ src/main/java/com/pai2/bank/app/dao/PersonDAO.java
@@ -1,5 +1,6 @@
 package com.pai2.bank.app.dao;
 
+import com.pai2.bank.app.model.Bankaccountoffer;
 import com.pai2.bank.app.model.Person;
 
 public interface PersonDAO extends Dao<Integer,Person> {
@@ -9,6 +10,7 @@ public interface PersonDAO extends Dao<Integer,Person> {
 //    public Set<Person> getPersonList();
     Person findPersonData(Integer userId);
 
+    public void save(Person person);
 
 
 }
